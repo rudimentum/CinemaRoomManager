4cbd5e03ef2a195de2c7a439c7496967b9a228f1
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-01-10 17:42:13 +0300

    2/5: Sold!
    Calculate the profit from the sold tickets depending on the number of seats and print the result.

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,23 +1,28 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
-        System.out.println("Cinema:");
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (i == 0 && j == 0) {
-                    System.out.print("  ");
-                } else if (i == 0) {
-                    System.out.print(j + " ");
-                } else if (j == 0){
-                    System.out.print(i + " ");
-                } else {
-                    System.out.print("S ");
-                }
-            }
-            System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row: ");
+        int seats = scanner.nextInt();
+        System.out.printf("Total income: $%d", calculateProfit(rows, seats));
+    }
+
+    public static int calculateProfit(int rows, int seats) {
+        int total = rows * seats;
+        int price;
+        if (total <= 60) {
+            price = total * 10;
+        } else {
+            int part = Math.round(rows/2);
+            price = (part * seats * 10) + ((rows - part) * seats * 8);
         }
+        return price;
     }
 }

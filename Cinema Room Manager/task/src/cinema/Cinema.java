e8709b0ae5408f5d8660525ddafb5b1be878be02
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-01-10 17:26:24 +0300

    1/5: Arrangement

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,6 +4,20 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
-
+        System.out.println("Cinema:");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == 0 && j == 0) {
+                    System.out.print("  ");
+                } else if (i == 0) {
+                    System.out.print(j + " ");
+                } else if (j == 0){
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print("S ");
+                }
+            }
+            System.out.println();
+        }
     }
 }

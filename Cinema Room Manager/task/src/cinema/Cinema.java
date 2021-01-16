2252fecef16e566268792ca884d5939572a9d823
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-01-17 01:49:24 +0300

    5/5: Statistics
    add statistics to program. The stats will show the current income, total income, the number of available seats, and the percentage of occupancy.

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -13,6 +13,7 @@ public class Cinema {
         while (true) {
             System.out.println("1. Show the seats\n" +
                     "2. Buy a ticket\n" +
+                    "3. Statistics\n" +
                     "0. Exit\n");
             int choice = scanner.nextByte();
             switch (choice) {
@@ -21,6 +22,9 @@ public class Cinema {
                     break;
                 case 2:
                     buyTicket(room);
+                    break;
+                case 3:
+                    printStatistics(room);
                     break;
                 case 0:
                     return;
@@ -60,22 +64,25 @@ public class Cinema {
     public static String[][] buyTicket(String [][] room) {
         Scanner scanner = new Scanner(System.in);
         try {
-            System.out.println("Enter a row number: ");
-            int row = scanner.nextInt();
-            System.out.println("Enter a seat number in that row: ");
-            int seat = scanner.nextInt();
-            int rows = room.length - 1;
-            int seats = room[rows].length - 1;
-            if (row > rows || seat > seats) {
-                System.out.printf("No such seat. There are %d rows of %d seats.\n", rows, seats);
-            } else if ("B ".equals(room[row][seat])) {
-                System.out.println("This seat is occupied! Choose another one!");
-            } else {
-                room[row][seat] = "B ";
-                System.out.println("Ticket price: $" + calculateTicketPrice(rows, seats, row));
+            while (true) {
+                System.out.println("Enter a row number: ");
+                int row = scanner.nextInt();
+                System.out.println("Enter a seat number in that row: ");
+                int seat = scanner.nextInt();
+                int rows = room.length - 1;
+                int seats = room[rows].length - 1;
+                if (row > rows || seat > seats) {
+                    System.out.printf("Wrong input!");
+                } else if ("B ".equals(room[row][seat])) {
+                    System.out.println("That ticket has already been purchased!");
+                } else {
+                    room[row][seat] = "B ";
+                    System.out.println("Ticket price: $" + calculateTicketPrice(rows, seats, row));
+                    break;
+                }
             }
         } catch (InputMismatchException e) {
-            System.out.println("Invalid input! Use integers!");
+            System.out.println("Wrong input! Use integers!");
         } finally {
            return room;
         }
@@ -89,5 +96,51 @@ public class Cinema {
             }
             System.out.println();
         }
+    }
+
+    public static int calculateIncome(String[][] room, int amount) {
+        int rows = room.length - 1;
+        int seats = room[rows].length - 1;
+        int total = rows * seats;
+        int part = Math.round(rows / 2);
+        int income = 0;
+        if (amount == total) {
+            if (amount <= 60) {
+                income = amount * 10;
+            } else {
+                income = (part * seats * 10) + ((rows - part) * seats * 8);
+            }
+        } else {
+            for (int i = 0; i < room.length; i++) {
+                for (int j = 0; j < room[i].length; j++) {
+                    if ("B ".equals(room[i][j])) {
+                        income = i <= part ? income + 10 : income + 8;
+                    }
+                }
+            }
+        }
+        return income;
+    }
+
+    public static int getPurchased(String[][] room) {
+        int purchased = 0;
+        for (int i = 0; i < room.length; i++) {
+            for (int j = 0; j < room[i].length; j++) {
+                purchased = "B ".equals(room[i][j]) ? ++purchased : purchased;
+            }
+        }
+        return purchased;
+    }
+
+    public static void printStatistics(String[][] room) {
+        int rows = room.length - 1;
+        int seats = room[rows].length - 1;
+        int total = rows * seats;
+        int purchased = getPurchased(room);
+        System.out.printf("Number of purchased tickets: %d%n", purchased);
+        float percentageOccupancy = (float) purchased / total * 100;
+        System.out.printf("Percentage: %.2f%%%n", percentageOccupancy);
+        System.out.printf("Current income: $%d%n", calculateIncome(room, purchased));
+        System.out.printf("Total income: $%d%n", calculateIncome(room, total));
     }
 }
